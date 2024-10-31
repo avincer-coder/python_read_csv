5849133b7b4cb364460da0270ad6accb4029e7bb
avincer-coder <gar23078@byui.edu>
2024-10-30 19:05:20 -0600

    done

--- receipt.py
+++ receipt.py
@@ -2,9 +2,7 @@ import pandas as pd
 
 def read_dictionary(filename, key_column_index):
         product_csv = pd.read_csv(filename)
-        # print(product_csv)
-        dict_product=product_csv.set_index(key_column_index).T.to_dict("list")
-        # print(dict_product)
+        dict_product = product_csv.set_index(product_csv.columns[key_column_index]).T.to_dict("list")
         return(dict_product)
         
 
@@ -12,23 +10,33 @@ def read_dictionary(filename, key_column_index):
 
 
 def main():
-        dict_product_from_read = read_dictionary("products.csv", "Product #")
-        # print(dict_product_from_read)
-
+        products_dict = read_dictionary("products.csv", 0)
+        print(products_dict)
         with open("request.csv", mode="r") as file:
                 next(file)
-
+                request_summary = {}
                 for request_line in file:
                         file_request_format = request_line.strip().split(",")
                         product_code = file_request_format[0]
+                        product_price = products_dict[product_code][1] 
+                        product_name = products_dict[product_code][0] 
                         product_quantity = int(file_request_format[1])
-                        product_price = dict_product_from_read[product_code][1]
-                        product_name = dict_product_from_read[product_code][0]
+                        print(f"{product_name} {product_quantity} ${product_price}") 
+                        if product_code in request_summary: 
+                                request_summary[product_code]+=product_quantity
+                        else:
+                                request_summary[product_code]=product_quantity
+                         
+                # for product_code in request_summary: 
+                #         if product_code in products_dict:
+                #                 product_name = products_dict[product_code][0]
+                #                 product_price = products_dict[product_code][1]
+                #                 total_quantity = request_summary[product_code]
+                #                 # print(f", {product_name}: {total_quantity} @ ${product_price:.2f} each")
+                #         else:
+                #                 # print(f"Product {product_code} not found in products list.") 
                         
-                        print(f"{product_name} {product_quantity} ${product_price}")
-
-                        if product_code in file_request_format:
-                                print(f"se repite el producto {product_name}")
                         
-                        print()
-main()
+        
+if __name__ == "__main__":
+    main()
